e81796a544314bfc65605ea530046b56b535c19c
Alexis Van Hoorde <a.vanhoorde@etu-webschoolfactory.fr>
2018-12-04 01:02:22 +0100

    configuration heroku apps

--- provisions/heroku.tf
+++ provisions/heroku.tf
@@ -1,3 +1,7 @@
+provider "heroku" {
+    email   = "${var.heroku_email}"
+    api_key = "${var.heroku_api_key}"
+}
 # Create new app
 resource "heroku_app" "staging" {
   name   = "stagingdevopswsf1"
